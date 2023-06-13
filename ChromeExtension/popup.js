75b8f34f3ff1a224317ec233d6ca538fb9d652ab
Paweł Gryka <pawel.m.gryka@gmail.com>
2023-06-13 10:37:38 +0200

    Add Chrome Extension files

---
+++ ChromeExtension/popup.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sendBtn = document.getElementById('sendBtn');
+    sendBtn.addEventListener('click', () => {
+        sendURL()
+    });
+});
+
+function asyncTabsQuery(query) {
+    return new Promise(resolve => {
+        chrome.tabs.query(query, resolve);
+    });
+}
+
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
+
+async function sendURL() {
+    const tabs = await asyncTabsQuery({active: true, currentWindow: true});
+    const currentURL = tabs[0].url;
+    const maxScrollTime = 5; // Set the desired max_scroll_time value here
+
+    const apiUrl = 'http://89.76.210.123:27019/check-place';
+    const queryString = '?url=' + encodeURIComponent(currentURL) + '&max_scroll_time=' + maxScrollTime;
+    const requestUrl = apiUrl + queryString;
+
+    const res = await fetch(requestUrl);
+    console.log(res.status);
+    const data = await res.json();
+    console.log(data);
+    const resultDiv = document.getElementById('result');
+    resultDiv.textContent = 'Waiting for results...\nEstimated wait time: ' + data.estimated_wait_time + ' seconds';
+    await sleep(data.estimated_wait_time * 1000);
+
+    const resultsId = data.task_id;
+    let isRunning = true;
+
+    const resultsUrl = `http://89.76.210.123:27019/check-results?results_id=${resultsId}`;
+    while (isRunning) {
+        const resultsRes = await fetch(resultsUrl);
+        const resultsData = await resultsRes.json();
+
+        console.log(resultsData);
+
+        if (resultsData.type === 'running') {
+          await sleep(3000); // Wait for 1 second before checking again
+        } else {
+          isRunning = false;
+          // Do something with the final results
+          console.log('Final results:', resultsData);
+          resultDiv.textContent = 'Fake percentage: ' + Math.round(resultsData.fake_checker_response.fake_percentage) +
+              '% \nAnything above 30% should be worrying';
+        }
+    }
+
+
+}
+
+
